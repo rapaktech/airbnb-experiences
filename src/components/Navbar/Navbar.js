6957184f53da410077b446491ab775dbbf59628d
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-06-17 15:57:09 +0100

    feat: added Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import airbnbLogo from '../../logo/airbnb-logo.png';
+
+export default function Navbar () {
+    return (
+        <nav>
+            <img src={airbnbLogo} alt='AirBnb Logo' />
+        </nav>
+    );
+}
