c93d27c6c790195f047eb55b3184bb21620440e3
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-06-19 13:18:48 +0100

    feat: modified card to use props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Card from './components/Card/Card';
+import personImage from './assets/images/katie-zaferes.png';
+import starIcon from './assets/icons/star-icon.png';
+import ellipseIcon from './assets/icons/ellipse-icon.png';
 
 export default function App () {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        personImage={personImage}
+        personImageAltText='Katie Zaferes'
+        availabilityStatusText='SOLD OUT'
+        starIcon={starIcon}
+        starIconAltText='Star Icon'
+        ellipseIcon={ellipseIcon}
+        ellipseIconAltText='Ellipse Icon'
+        averageStarRating='5.0'
+        numberOfRatings='6'
+        countryAbbreviation='USA'
+        title='Life lessons with Katie Zaferes'
+        price='From $136 / person'
+      />
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,19 @@
 import './Card.css';
 import React from "react";
-import personImage from '../../assets/images/katie-zaferes.png';
-import starIcon from '../../assets/images/star-icon.png';
-import ellipseIcon from '../../assets/images/ellipse-icon.png';
 
-export default function Card () {
+export default function Card (props) {
     return (
         <section className='card'>
-            <img src={personImage} alt='Katie Zaferes' className='card--person-image' />
+            <img src={props.personImage} alt={props} className='card--person-image' />
             <div className='card--experience-availabilty-text-container'></div>
-            <p className='card--experience-availabilty-text'>SOLD OUT</p>
-            <img src={starIcon} alt='Sta Icon' className='card--star-icon' />
-            <img src={ellipseIcon} alt='Ellipse Icon' className='card--ellipse-icon' />
-            <h6 className='card--average-star-rating'>5.0</h6>
-            <h6 className='card--number-of-ratings'>(6)</h6>
-            <h6 className='card--country-abbreviation'>USA</h6>
-            <h6 className='card--experience-title'>Life lessons with Katie Zaferes</h6>
-            <p className='card--experience-price'>From $136 / person</p>
+            <p className='card--experience-availabilty-text'>{props.availabilityStatusText}</p>
+            <img src={props.starIcon} alt={props.starIconAltText} className='card--star-icon' />
+            <img src={props.ellipseIcon} alt={props.ellipseIconAltText} className='card--ellipse-icon' />
+            <h6 className='card--average-star-rating'>{props.averageStarRating}</h6>
+            <h6 className='card--number-of-ratings'>({props.numberOfRatings})</h6>
+            <h6 className='card--country-abbreviation'>{props.countryAbbreviation}</h6>
+            <h6 className='card--experience-title'>{props.title}</h6>
+            <p className='card--experience-price'>{props.price}</p>
         </section>
     )
 }
